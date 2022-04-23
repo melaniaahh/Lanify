9e89f63438440fd85a14eb4465af2d08e240acaa
Melania Anggraini Hapsari <melaniaanggraini@mail.ugm.ac.id>
2022-04-23 12:01:40 +0700

    Add: Auth & Playlist

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
-import './App.css';
+import "./App.css";
 import NavigationBar from "./components/NavigationBar";
 import Hero from "./components/Hero";
-import SignUpForm from './components/SignUpForm';
+// Firebase sedang dalam pengembangan
+// import SignUpForm from "./components/SignUpForm";
+import SpotifyAuth from "./pages/SpotifyAuth";
 
-const App = () => {
+const App = () => { 
   return (
   <Router>
     <NavigationBar />
-
     <Switch>
-      <Route path={ROUTES.SIGN_UP} component={SignUpForm}/>
       <Route path={ROUTES.HOME} component={Hero}/>
     </Switch>
+    <SpotifyAuth />
   </Router>
+
   );
 };
 
